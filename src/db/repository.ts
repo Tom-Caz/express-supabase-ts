325a3de7bb616603a18b24d9d08aa1a02860e5e0
Thomas Cazort <thomas.cazort@gmail.com>
2024-03-08 16:09:43 -0800

    implement supabase

--- src/db/repository.ts
+++ src/db/repository.ts
@@ -6,3 +6,17 @@ const client = createClient(
     process.env.SUPABASE_URL as string,
     process.env.SUPABASE_ANON_KEY as string,
 );
+
+const getAll = async () => {
+    const { data, error } = await client
+        .from('todos')
+        .select('*');
+    if (error) {
+        throw error;
+    }
+    return data;
+}
+
+const db = { getAll };
+
+export default db;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import express, { Application, Request, Response } from "express";
 import { config } from "dotenv";
+import db from "./db/repository";
 config({ path: '.env.local' });
 
 const app: Application = express();
 const port = 3001;
 
+
 app.get("/", (_: Request, res: Response): Response => {
     return res.status(200).send({
         message: "ALIVE",
+    });
+});
+
+
+app.get("/todos", async (_: Request, res: Response): Promise<Response> => {
+    const todos = await db.getAll();
+    return res.status(200).send({
+        todos: todos,
     });
 });
 
